5cb8d4c57767861accccf9ca2003c3498a5447a0
Alex Ssanya <alexssanya@gmail.com>
2019-07-25 10:27:11 +0300

    chore(project structure) : setting up project structure - set up the project structure - install necessary project dependencies.

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+var path = require('path');
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    publicPath: '/'
+  },
+  resolve: {
+    enforceExtension: false,
+    descriptionFiles: ['package.json'],
+    extensions: ['.js', '.jsx', '.json', '.tsx', '.ts','.scss','.css','sass'],
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+      },
+    },
+    {
+      test: /\.(scss|css)$/,
+      use: ['style-loader', // creates style nodes from JS strings
+        'css-loader', // translates CSS into CommonJS
+        'sass-loader', // compiles Sass to CSS, using Node Sass by default
+      ],
+    },
+    {
+      test: /\.html$/,
+      use: [{
+        loader: 'html-loader',
+      }],
+    },
+    {
+      test: /\.(jpg|png|gif|svg|pdf|ico)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name]-[hash:8].[ext]',
+          },
+        },
+      ],
+    },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+};
